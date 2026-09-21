101236330b1320de66639b1c50a5dd807cc7e6ee
junland <unlandj2012@gmail.com>
2026-09-21 15:18:21 -0500

    Refactor IAM policy documents for EKS cluster and node group roles; consolidate definitions and improve structure

--- _data.tf
+++ _data.tf
@@ -10,3 +10,79 @@ data "aws_eks_cluster_auth" "this" {
 data "tls_certificate" "eks_oidc" {
   url = aws_eks_cluster.this.identity[0].oidc[0].issuer
 }
+
+data "aws_iam_policy_document" "eks_cluster_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "eks_node_group_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "gitlab_irsa_assume_role" {
+  count = local.manage_irsa_role ? 1 : 0
+
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.this.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.this.url, "https://", "")}:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "${replace(aws_iam_openid_connect_provider.this.url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:${var.gitlab_namespace}:*"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "gitlab_irsa_s3" {
+  count = local.manage_irsa_role && var.irsa_policy_json == null ? 1 : 0
+
+  statement {
+    sid = "GitLabObjectStorageList"
+
+    actions = [
+      "s3:ListBucket",
+      "s3:GetBucketLocation"
+    ]
+
+    resources = [for bucket in values(local.s3_bucket_names) : "arn:${data.aws_partition.current.partition}:s3:::${bucket}"]
+  }
+
+  statement {
+    sid = "GitLabObjectStorageRW"
+
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:DeleteObject",
+      "s3:AbortMultipartUpload",
+      "s3:ListMultipartUploadParts"
+    ]
+
+    resources = [for bucket in values(local.s3_bucket_names) : "arn:${data.aws_partition.current.partition}:s3:::${bucket}/*"]
+  }
+}
--- _iam.tf
+++ _iam.tf
@@ -1,14 +1,3 @@
-data "aws_iam_policy_document" "eks_cluster_assume_role" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["eks.amazonaws.com"]
-    }
-  }
-}
-
 resource "aws_iam_role" "eks_cluster" {
   name               = "${local.cluster_name}-cluster-role"
   assume_role_policy = data.aws_iam_policy_document.eks_cluster_assume_role.json
@@ -23,17 +12,6 @@ resource "aws_iam_role_policy_attachment" "eks_cluster" {
 
   role       = aws_iam_role.eks_cluster.name
   policy_arn = each.value
-}
-
-data "aws_iam_policy_document" "eks_node_group_assume_role" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-  }
 }
 
 resource "aws_iam_role" "eks_node_group" {
@@ -61,66 +39,12 @@ resource "aws_iam_openid_connect_provider" "this" {
   tags = local.tags
 }
 
-data "aws_iam_policy_document" "gitlab_irsa_assume_role" {
-  count = local.manage_irsa_role ? 1 : 0
-
-  statement {
-    actions = ["sts:AssumeRoleWithWebIdentity"]
-
-    principals {
-      type        = "Federated"
-      identifiers = [aws_iam_openid_connect_provider.this.arn]
-    }
-
-    condition {
-      test     = "StringEquals"
-      variable = "${replace(aws_iam_openid_connect_provider.this.url, "https://", "")}:aud"
-      values   = ["sts.amazonaws.com"]
-    }
-
-    condition {
-      test     = "StringLike"
-      variable = "${replace(aws_iam_openid_connect_provider.this.url, "https://", "")}:sub"
-      values   = ["system:serviceaccount:${var.gitlab_namespace}:*"]
-    }
-  }
-}
-
 resource "aws_iam_role" "gitlab_irsa" {
   count = local.manage_irsa_role ? 1 : 0
 
   name               = local.irsa_role_name
   assume_role_policy = data.aws_iam_policy_document.gitlab_irsa_assume_role[0].json
   tags               = local.tags
-}
-
-data "aws_iam_policy_document" "gitlab_irsa_s3" {
-  count = local.manage_irsa_role && var.irsa_policy_json == null ? 1 : 0
-
-  statement {
-    sid = "GitLabObjectStorageList"
-
-    actions = [
-      "s3:ListBucket",
-      "s3:GetBucketLocation"
-    ]
-
-    resources = [for bucket in values(local.s3_bucket_names) : "arn:${data.aws_partition.current.partition}:s3:::${bucket}"]
-  }
-
-  statement {
-    sid = "GitLabObjectStorageRW"
-
-    actions = [
-      "s3:PutObject",
-      "s3:GetObject",
-      "s3:DeleteObject",
-      "s3:AbortMultipartUpload",
-      "s3:ListMultipartUploadParts"
-    ]
-
-    resources = [for bucket in values(local.s3_bucket_names) : "arn:${data.aws_partition.current.partition}:s3:::${bucket}/*"]
-  }
 }
 
 resource "aws_iam_role_policy" "gitlab_irsa" {
